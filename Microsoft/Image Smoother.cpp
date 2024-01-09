341aabc2c4e33aa350bdcc2b99f689d7d70739b7
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-09 23:22:53 +0530

    5Qs Microsoft

---
+++ Microsoft/Image Smoother.cpp
@@ -0,0 +1,33 @@
+// Problem Link: https://leetcode.com/problems/image-smoother/description/
+
+class Solution {
+public:
+    int bfs(int row, int col, int n, int m, vector<vector<int>>& img) {
+        int sum = 0, count = 0;
+
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                int nrow = row + i;
+                int ncol = col + j;
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
+                    sum += img[nrow][ncol];
+                    count++;
+                }
+            }
+        }
+        return sum / count;
+    }
+
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n = img.size(), m = img[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                ans[i][j] = bfs(i, j, n, m, img);
+            }
+        }
+        return ans;
+    }
+};
---
+++ Microsoft/Wiggle Sort II.cpp
@@ -0,0 +1,21 @@
+// Problem Link: https://leetcode.com/problems/wiggle-sort-ii/description/
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        int i = 1, j = 0;
+        priority_queue<int> q(nums.begin(), nums.end());
+
+        while(i < n && !q.empty()) {
+            nums[i] = q.top();
+            q.pop();
+            i += 2;
+        }
+        while(j < n && !q.empty()) {
+            nums[j] = q.top();
+            q.pop();
+            j += 2;
+        }
+    }
+};
