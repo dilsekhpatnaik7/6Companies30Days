a4e3cf702f6af2997ff177874e7e25c9d85197aa
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-15 00:01:48 +0530

    Some more Qs Walmart

---
+++ Walmart/Extra Characters in a String.cpp
@@ -0,0 +1,31 @@
+// Problem Link: https://leetcode.com/problems/extra-characters-in-a-string/description/
+
+class Solution {
+public:
+    int solve(int start, int end, string s, unordered_map<string, int>& m, vector<int>& dp) {
+        if (end >= s.size()) return end - start;
+        if (dp[start] != -1) return dp[start];
+
+        string temp = s.substr(start, end - start + 1);
+        int pick = 1e9;
+
+        if(m.find(temp) != m.end()) {
+            pick = solve(end + 1, end + 1, s, m, dp);
+        }
+
+        int notPick = solve(start, end + 1, s, m, dp);
+        int skip = end - start + 1 + solve(end + 1, end + 1, s, m, dp);
+
+        return dp[start] = min({pick, notPick, skip});
+    }    
+
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_map<string, int> m;
+        for(auto i: dictionary) {
+            m[i]++;
+        }
+        int n = s.size();
+        vector<int> dp(n + 1, -1);
+        return solve(0, 0, s, m, dp);
+    }
+};
---
+++ Walmart/Rotate Function.cpp
@@ -0,0 +1,20 @@
+// Problem Link: https://leetcode.com/problems/rotate-function/description/
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        long long arraySum = 0, original = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            arraySum += nums[i];
+            original += (i * nums[i]);
+        }
+
+        long long maximumSum = original;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            original += arraySum - (nums.size() * nums[i]);
+            maximumSum = max(original, maximumSum);
+        }
+        return maximumSum;
+    }
+};
