2141de8c49ae606572fb0220b250941b84d0f4d5
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-15 12:32:22 +0530

    some more Walmart

---
+++ Walmart/K-diff Pairs in an Array.cpp
@@ -0,0 +1,17 @@
+// Problem Link: https://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+
+        int pairCount = 0;
+        for(auto i: m) {
+            if((!k && i.second > 1) || (k && m.count(i.first + k))) pairCount++;
+        }
+        return pairCount;
+    }
+};
