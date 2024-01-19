3182f5c6279f795379fbe84c9224f8e6f4645fb7
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-19 23:40:31 +0530

    Some Adobe Qs

---
+++ Adobe/Construct the Longest New String.cpp
@@ -0,0 +1,24 @@
+// Problem Link: https://leetcode.com/problems/construct-the-longest-new-string/description/
+
+class Solution {
+public:
+    int longestString(int x, int y, int z) {
+        int aStart = 0, bStart = 0;
+        int X = x, Y = y;
+
+        while(X && Y) {
+            aStart += 2;
+            bStart += 2;
+            X--, Y--;
+        }
+
+        if(X) aStart++;
+        if(Y) bStart++;
+
+        int ans = 0;
+        ans = max(aStart, bStart);
+        ans += z;
+
+        return 2 * ans;
+    }
+};
