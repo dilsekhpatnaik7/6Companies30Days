a0d04ab90766a4e4a7da2bd7de405beadc89768e
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-30 16:23:23 +0530

    completed

---
+++ Google/Maximum Product After K Increments.cpp
@@ -0,0 +1,28 @@
+// Problem Link: https://leetcode.com/problems/maximum-product-after-k-increments/description/
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    int maximumProduct(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0;i < nums.size(); i++){
+            pq.push(nums[i]);
+        }
+
+        while(k > 0) {
+            int ans = pq.top();
+            pq.pop();
+            ans++;
+            pq.push(ans);
+            k--;
+        }
+        long long product = 1;
+        while(pq.empty() == false){
+            product *= pq.top();
+            product = product % mod;
+            pq.pop();
+        }
+        return product;
+    }
+};
