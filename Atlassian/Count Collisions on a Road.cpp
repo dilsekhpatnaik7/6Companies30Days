69421e39a2a00acf59898b0acfb79c6bcb1c3074
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-30 10:25:00 +0530

    atlassian done

---
+++ Atlassian/Count Collisions on a Road.cpp
@@ -0,0 +1,17 @@
+// Problem Link: https://leetcode.com/problems/count-collisions-on-a-road/description/
+
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int left = 0, right = directions.size() - 1;
+
+        while(left < directions.size() && directions[left] == 'L') left++;
+        while(right >= 0 && directions[right] == 'R') right--;
+
+        int ans = 0;
+        for(int i = left; i <= right; i++) {
+            if(directions[i] != 'S') ans++;
+        }
+        return ans;
+    }
+};
